d118aad716b88adc0cade1a9b480073aab63668a
Miracle <ssum1913@163.com>
2023-11-26 20:19:16 +0800

    Redis环境配置

---
+++ src/main/java/com/itheima/reggie/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.itheima.reggie.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Rides配置类
+ */
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //默认的Key序列化器JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        return redisTemplate;
+
+    }
+}
